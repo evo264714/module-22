853015648340414bc5e08bf4e1e0a9bdb4268bef
evo264714 <mh264714@gmail.com>
2023-02-02 17:26:27 +0600

    math

---
+++ math.js
@@ -0,0 +1,40 @@
+const result = Math.pow(3, 8);
+// console.log(result);
+
+const num1 = 25;
+const num2 = 45;
+
+const gap = Math.abs(num1 - num2);
+// console.log(gap);
+if (gap < 5){
+    console.log("You guys can be friends");
+}
+else{
+    console.log("you guys stay apart");
+}
+
+// *---------------------------
+
+// *---------------------------
+
+const number = 2.4598;
+const fullNumber = Math.round(number);
+// console.log(fullNumber);
+const result2 = Math.ceil(2.00002);
+console.log(result2);
+const result3 = Math.floor(456.259);
+console.log(result3);
+
+// *------------------
+
+// *------------------
+
+// console.log(Math.random());
+
+const random = Math.round(Math.random()*100);
+// console.log(random);
+
+for(let i = 1; i <= 1; i++){
+    const random = Math.round(Math.random()*6);
+    console.log(random);
+}
